3b722ac7dbba68f5b46b5f4baf876648ef88497d
ikramsadki <98346094+ikramsadki@users.noreply.github.com>
2023-02-09 16:04:41 +0100

    Update ChatGptConfig.java

--- chatgpt-service/src/main/java/com/app/config/ChatGptConfig.java
+++ chatgpt-service/src/main/java/com/app/config/ChatGptConfig.java
@@ -4,7 +4,7 @@ public class ChatGptConfig {
 
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
